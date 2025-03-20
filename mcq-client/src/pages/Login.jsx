4fac1015f38d2151d269a1035986cab8b81c55d8
Dilshad <dilshad360.dev@gmail.com>
2025-03-20 11:43:59 +0530

    fix: login

--- mcq-client/src/pages/Login.jsx
+++ mcq-client/src/pages/Login.jsx
@@ -1,11 +1,9 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { useNavigate } from 'react-router-dom';
 import { login } from '../../services/api';
 import { useAuth } from '../context/authContext';
 
 const Login = () => {
-   
 
     const { login: authLogin } = useAuth();
 
@@ -28,27 +26,26 @@ const Login = () => {
     
 
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <div className="card w-96 shadow-xl">
+        <div className="min-h-screen flex flex-col items-center justify-center">
+                    <h2 className="card-title text-[32px] text-primary pb-3">Login</h2>
+            <div className="card w-[440px] shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title">Login</h2>
                     <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-                        <Form>
-                            <div className="form-control">
-                                <label>Mobile</label>
-                                <Field name="mobile" className="input input-bordered" />
+                        <Form className='space-y-4' >
+                            <div className="form-control flex flex-col gap-1">
+                                <label className='text-xl font-semibold' >Mobile Number</label>
+                                <Field placeholder="Enter your mobile number" name="mobile" className="input input-bordered w-full" />
                                 <ErrorMessage name="mobile" component="div" className="text-red-500 text-sm" />
                             </div>
-
-                            <div className="form-control">
-                                <label>Password</label>
-                                <Field name="password" type="password" className="input input-bordered" />
+                            <div className="form-control flex flex-col gap-1">
+                                <label className='text-xl font-semibold' >Password</label>
+                                <Field placeholder="Enter password" name="password" type="password" className="input input-bordered w-full" />
                                 <ErrorMessage name="password" component="div" className="text-red-500 text-sm" />
                             </div>
-
                             <button type="submit" className="btn btn-primary w-full mt-4">Login</button>
                         </Form>
                     </Formik>
+                    <span className='text-center pt-4'>Don’t have an account? <a className='text-[#006eec]' href="/register"> Register Now </a></span>
                 </div>
             </div>
         </div>
